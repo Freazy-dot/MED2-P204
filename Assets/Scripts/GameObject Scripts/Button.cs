7033c463057bcf42759e8b4043c5603db8b5942c
tikzyy <145325630+tikzyy@users.noreply.github.com>
2024-04-22 14:33:55 +0200

    Update Button.cs & Timer.cs

--- Assets/Scripts/GameObject Scripts/Button.cs
+++ Assets/Scripts/GameObject Scripts/Button.cs
@@ -1,41 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Button : MonoBehaviour, IInteractable
 {
     public Color buttonColor;
-    private Timer timer;
-    public static Dictionary<Color, int> buttonCounts = new Dictionary<Color, int>();
-    public static HashSet<Color> matchedColors = new HashSet<Color>();
-    private bool hasBeenPressed;
+    public ButtonManager buttonManager;
 
     public void Interact(GameObject player)
     {
         Debug.Log("Button pressed: " + buttonColor);
-        
-        timer = player.GetComponent<Timer>();
+        buttonManager.HandleButtonPress(buttonColor);
+    }
+}
 
-        if (timer.timeLeft <= 0) 
-        {
-            if (!hasBeenPressed)
-            {
-                IncrementButtonCount(buttonColor);
-                hasBeenPressed = true;
-            }
-            timer.StartTimer();
-            return;
-        }
+public class ButtonManager : MonoBehaviour
+{
+    private Dictionary<Color, int> buttonCounts = new Dictionary<Color, int>();
+    private HashSet<Color> matchedColors = new HashSet<Color>();
+    private Timer timer;
 
-        if (!hasBeenPressed)
-        {
-            IncrementButtonCount(buttonColor);
-            hasBeenPressed = true;
-        }
+    private void Start()
+    {
+        timer = GetComponent<Timer>();
     }
 
-    private void IncrementButtonCount(Color color)
+    public void HandleButtonPress(Color color)
     {
+        if (timer.timeLeft <= 0) 
+        {
+            timer.StartTimer();
+            ResetButtonCounts();
+        }
+
         if (!buttonCounts.ContainsKey(color))
         {
             buttonCounts[color] = 0;
@@ -43,18 +41,26 @@ public class Button : MonoBehaviour, IInteractable
 
         buttonCounts[color]++;
 
-        // Check if the player has pressed 3 buttons of the same color
         if (buttonCounts[color] == 3)
         {
             Debug.Log("Pressed 3 buttons of the same color: " + color);
             matchedColors.Add(color);
-            buttonCounts[color] = 0; // Reset the count for this color
-            hasBeenPressed = false;
+            buttonCounts[color] = 0;
 
-            // Check if all three colors have been matched
             if (matchedColors.Count == 3)
             {
                 Debug.Log("All three colors have been matched!");
+            }
+        }
+    }
+
+    public void ResetButtonCounts()
+    {
+        foreach (Color color in buttonCounts.Keys.ToList())
+        {
+            if (!matchedColors.Contains(color))
+            {
+                buttonCounts[color] = 0;
             }
         }
     }
