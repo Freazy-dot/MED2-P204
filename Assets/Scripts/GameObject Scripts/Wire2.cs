cc6eb1a28e8feb67ae03a682415a0cf084e33c73
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2024-04-29 18:52:55 +0200

    har problemer med delay/corountine

--- Assets/Scripts/GameObject Scripts/Wire2.cs
+++ Assets/Scripts/GameObject Scripts/Wire2.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using static Unity.Burst.Intrinsics.X86.Avx;
 
 
 public class Wire2 : MonoBehaviour
 {
-    [SerializeField] GameObject boxFront, boxBottom, boxRight, boxLeft, boxTop;
+    //[SerializeField] GameObject boxFront, boxBottom, boxRight, boxLeft, boxTop;
     [SerializeField] GameObject wireRed, wireGreen, wireBlue, wireYellow, wirePurple, wirePink;
-    [SerializeField] Material material;
+    Animator animator;
+
+   // [SerializeField] Material material;
 
         void Start()
     {
-        
-                boxFront.GetComponent<Rigidbody>().useGravity = false;
-                boxBottom.GetComponent<Rigidbody>().useGravity = false;
+            animator = GetComponent<Animator>();
+       
+             //   boxFront.GetComponent<Rigidbody>().useGravity = false;
+            //    boxBottom.GetComponent<Rigidbody>().useGravity = false;
     }
 
   
@@ -22,28 +26,61 @@ public class Wire2 : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-           boxFront.GetComponent<Rigidbody>().useGravity = true;
-           boxBottom.GetComponent<Rigidbody>().useGravity = true;
+
+            OpenBox();
+
+           
+            
+
+
+
+         //  boxFront.GetComponent<Rigidbody>().useGravity = true;
+         //  boxBottom.GetComponent<Rigidbody>().useGravity = true;
 
            // boxFront.SetActive (false);
            // boxBottom.SetActive (false);
 
-            wireRed.SetActive(true);
-            wireGreen.SetActive(true);
-            wireBlue.SetActive(true);
-            wireYellow.SetActive(true);
-            wirePurple.SetActive(true);
-            wirePink.SetActive(true);
+            
 
-            boxRight.GetComponent<Renderer>().material = material;
-            boxLeft.GetComponent<Renderer>().material = material;
-            boxTop.GetComponent<Renderer>().material = material;
+          //  boxRight.GetComponent<Renderer>().material = material;
+          //  boxLeft.GetComponent<Renderer>().material = material;
+           // boxTop.GetComponent<Renderer>().material = material;
         }
+    }
+
+
+    public void OpenBox()
+    {
+        StartCoroutine(MyCoroutine());
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        animator.SetTrigger("Open");
+
+        yield return 6000;
+
+        ActivateWires();
+
+    }
+
+    public static async void Delay(int tmp)
+    {
+        await Task.Delay(tmp);
+
+
     }
 
     public void ActivateWires()
     {
+        
 
+         wireRed.SetActive(true);
+         wireGreen.SetActive(true);
+         wireBlue.SetActive(true);
+         wireYellow.SetActive(true);
+         wirePurple.SetActive(true);
+         wirePink.SetActive(true);
     }
 
 
