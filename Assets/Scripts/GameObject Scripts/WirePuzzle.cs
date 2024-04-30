29704b3ce0352a4eb594e62bf374775d699db1b5
Otter <clamarped@gmail.com>
2024-04-30 13:56:19 +0200

    idk

--- Assets/Scripts/GameObject Scripts/WirePuzzle.cs
+++ Assets/Scripts/GameObject Scripts/WirePuzzle.cs
@@ -7,19 +7,28 @@ public class WirePuzzle : MonoBehaviour
 {
     [SerializeField] int lightsOn;
     public Animator animator;
-    [SerializeField] Wire1 wire1;
+   // [SerializeField] Wire1 wire1;
     [SerializeField] GameObject red, green, yellow, blue, purple, pink;
 
 
     private void Start()
     {
-        wire1 = GetComponent<Wire1>();
+        red.GetComponent<Wire1>();
+        green.GetComponent<Wire1>();
+        yellow.GetComponent<Wire1>();
+        blue.GetComponent<Wire1>();
+        purple.GetComponent<Wire1>();
+        pink.GetComponent<Wire1>();
+
+
         animator = GetComponent<Animator>();
         lightsOn = 0;
     }
 
     private void Update()
     {
+       
+
         if (lightsOn >=6)
         {
             animator.SetTrigger("open");
