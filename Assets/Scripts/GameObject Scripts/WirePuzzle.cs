559bf6a401db44694cfa02c8fda09974c2e60fa7
Otter <clamarped@gmail.com>
2024-04-30 14:25:02 +0200

    error udkommenteret

--- Assets/Scripts/GameObject Scripts/WirePuzzle.cs
+++ Assets/Scripts/GameObject Scripts/WirePuzzle.cs
@@ -8,17 +8,17 @@ public class WirePuzzle : MonoBehaviour
     [SerializeField] int lightsOn;
     public Animator animator;
    // [SerializeField] Wire1 wire1;
-    [SerializeField] GameObject red, green, yellow, blue, purple, pink;
+   // [SerializeField] GameObject red, green, yellow, blue, purple, pink;
 
 
     private void Start()
     {
-        red.GetComponent<Wire1>();
-        green.GetComponent<Wire1>();
-        yellow.GetComponent<Wire1>();
-        blue.GetComponent<Wire1>();
-        purple.GetComponent<Wire1>();
-        pink.GetComponent<Wire1>();
+       // red.GetComponent<Wire1>();
+       // green.GetComponent<Wire1>();
+       // yellow.GetComponent<Wire1>();
+      //  blue.GetComponent<Wire1>();
+      //  purple.GetComponent<Wire1>();
+       // pink.GetComponent<Wire1>();
 
 
         animator = GetComponent<Animator>();
