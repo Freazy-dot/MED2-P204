69ca69782f8e9543bad089586bbc059dccdc02ef
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2024-04-30 10:05:46 +0200

    but

--- Assets/Scripts/GameObject Scripts/Button.cs
+++ Assets/Scripts/GameObject Scripts/Button.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 
 public class Button : MonoBehaviour, IInteractable
 {
+    SoundManager Soundman;
     public Color buttonColor;
     public ButtonManager buttonManager;
+
+    private void Start()
+    {
+        Soundman = GameObject.FindGameObjectWithTag("AudioMan").GetComponent<SoundManager>();
+    }
 
     public void Interact(GameObject player)
     {
         Debug.Log("Button pressed: " + buttonColor);
         buttonManager.HandleButtonPress(buttonColor, player);
+        Soundman.playSFX("Button_click");
     }
 }
