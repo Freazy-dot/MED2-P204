4967e83649776bb8de02d72bc508a740d3fccef4
Otter <clamarped@gmail.com>
2024-05-01 12:02:23 +0200

    script comments

--- Assets/Scripts/GameObject Scripts/BatteryButton.cs
+++ Assets/Scripts/GameObject Scripts/BatteryButton.cs
@@ -4,32 +4,14 @@ using UnityEngine;
 
 public class BatteryButton : MonoBehaviour
 {
-   // private bool batteryCollided = false;
     [SerializeField] Animator animator;
 
-   /* private void Update()
-    {
-        if (batteryCollided)
-        {
-            Debug.Log("Battery found!");
-        }
-        if(!batteryCollided)
-        {
-            Debug.Log("Battery not found!");
-        }   
-    } */
-
+    /// <summary>
+    /// once battery is removed from the button it triggers the colour code animation
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionExit(Collision collision)
     {
         animator.SetTrigger("Open");
     }
-
-
-   /* private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Battery"))
-        {
-            batteryCollided = true;
-        }
-    }*/
 }
