bc4df2e1fcd0620f36f7810f30a79489d7df382e
tikzyy <145325630+tikzyy@users.noreply.github.com>
2024-05-03 11:15:43 +0200

    fixed interaction

--- Assets/Scripts/Player Scripts/PlayerInteraction.cs
+++ Assets/Scripts/Player Scripts/PlayerInteraction.cs
@@ -12,33 +12,35 @@ public class PlayerInteraction : MonoBehaviour
 
     Transform playerCamera; //Reference to the camera
 
+    public LayerMask ignorePlayerHitbox;
+
     public void Awake()
     {
         playerCamera = GameObject.FindWithTag("PCCamera").GetComponent<Camera>().transform;
     }
 
-public void Update()
-{
-    Ray ray = new Ray(transform.position + new Vector3(0f, InteractionRayOffset, 0f), playerCamera.transform.forward);
-    RaycastHit hit;
+    public void Update()
+    {
+        Ray ray = new Ray(transform.position + new Vector3(0f, InteractionRayOffset, 0f), playerCamera.transform.forward);
+        RaycastHit hit;
 
-    IHighlightable highlightable = null;
-    if (!Physics.Raycast(ray, out hit, rayDistance)) {
-        return;
+        IHighlightable highlightable = null;
+        if (!Physics.Raycast(ray, out hit, rayDistance, ~ignorePlayerHitbox)) {
+            return;
+        }
+
+        highlightable = hit.collider.gameObject.GetComponent<IHighlightable>();
+
+        if (_highlightInteractable as MonoBehaviour != null && _highlightInteractable != highlightable) {
+            _highlightInteractable.OnLookAway();
+        }
+
+        if (highlightable != null) {
+            highlightable.OnLookAt();
+        }
+
+        _highlightInteractable = highlightable;
     }
-
-    highlightable = hit.collider.gameObject.GetComponent<IHighlightable>();
-
-    if (_highlightInteractable as MonoBehaviour != null && _highlightInteractable != highlightable) {
-        _highlightInteractable.OnLookAway();
-    }
-
-    if (highlightable != null) {
-        highlightable.OnLookAt();
-    }
-
-    _highlightInteractable = highlightable;
-}
 
     public void HandleInteraction()
     {
